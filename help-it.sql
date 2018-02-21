7e34797063ae970f3ce10b3e732e3571ad0028d5
zdienos <zdienos@yahoo.com>
2018-02-22 07:44:21 +0800

    update database

--- help-it.sql
+++ help-it.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Feb 17, 2018 at 10:06 AM
+-- Generation Time: Feb 22, 2018 at 12:43 AM
 -- Server version: 5.6.35
 -- PHP Version: 7.1.6
 
@@ -28,15 +28,6 @@ CREATE TABLE `ci_sessions` (
   `user_data` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `ci_sessions`
---
-
-INSERT INTO `ci_sessions` (`session_id`, `ip_address`, `user_agent`, `last_activity`, `user_data`) VALUES
-('1ad722b4152dac95b25ef0391367ba36', '::1', 'Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.101 Safari/537.36', 1428339021, ''),
-('6b6478c492b0fe2e504f291750a24d1c', '::1', 'Mozilla/5.0 (Windows NT 6.2; WOW64; rv:22.0) Gecko/20100101 Firefox/22.0', 1428344604, 'a:3:{s:9:\"user_data\";s:0:\"\";s:7:\"id_user\";s:1:\"2\";s:4:\"user\";s:1:\"2\";}'),
-('f19d98e1408babe7700aead53737bebf', '::1', 'Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.101 Safari/537.36', 1428345373, 'a:3:{s:9:\"user_data\";s:0:\"\";s:7:\"id_user\";s:1:\"1\";s:5:\"admin\";s:1:\"1\";}');
-
 -- --------------------------------------------------------
 
 --
@@ -53,14 +44,9 @@ CREATE TABLE `tb_departemen` (
 --
 
 INSERT INTO `tb_departemen` (`id_departemen`, `nama_departemen`) VALUES
-(1, 'IT'),
-(2, 'Operation'),
-(3, 'Engineering'),
-(4, 'Production Facilities'),
-(5, 'Marketing Commercial'),
-(6, 'Sales Corporate'),
-(7, 'Sales Edutainment'),
-(8, 'Show');
+(2, 'Food n Beverages'),
+(3, 'Creative'),
+(5, 'Taswer');
 
 -- --------------------------------------------------------
 
@@ -82,12 +68,44 @@ CREATE TABLE `tb_identifikasi` (
 --
 
 INSERT INTO `tb_identifikasi` (`id_identifikasi`, `tanggal`, `identifikasi`, `status`, `persentase`, `oleh`) VALUES
-('PK-00001', '2018-02-17', 'Oke siapp', 'On Progress', '75%', 'Mardino Santosa'),
-('PK-00002', '2018-02-15', 'siap laksanakannn\r\nssss\r\nzzz', 'Finished', '100%', 'Mardino Santosa'),
-('PK-00003', '2018-02-14', 'siapp', 'Finished', '75%', 'Fajri'),
-('PK-00004', '2018-02-17', 'harusnya ini sudah 25%\r\n50\r\nppp', 'Finished', '100%', 'Mardino Santosa'),
-('PK-00005', '2018-02-16', 'power supply ngadat', 'On Progress', '25%', 'Fajri'),
-('PK-00007', '2018-02-17', 'sudah saya identifikasi\r\nupdate\r\nlagi', 'Finished', '100%', 'Mardino Santosa');
+('PK-00001', '2018-02-20', 'sudah identifikasi nih\r\nlagi', 'Finished', '100%', 'Mardino Santosa'),
+('PK-00002', '2018-02-20', 'werwerwerwer', 'Finished', '100%', 'Mardino Santosa'),
+('PK-00003', '2018-02-20', 'werwe', 'On Progress', '25%', 'Mardino Santosa'),
+('PK-00007', '2018-02-20', 'ertert', 'Finished', '100%', 'Mardino Santosa');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_jadwal`
+--
+
+CREATE TABLE `tb_jadwal` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `color` varchar(7) NOT NULL,
+  `start` datetime NOT NULL,
+  `end` datetime DEFAULT NULL,
+  `allDay` varchar(50) NOT NULL DEFAULT 'true'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tb_jadwal`
+--
+
+INSERT INTO `tb_jadwal` (`id`, `title`, `description`, `color`, `start`, `end`, `allDay`) VALUES
+(24, 'Off', '', '#2f9550', '2018-01-28 00:00:00', '2018-01-28 00:00:00', 'true'),
+(25, 'PH/Cuti', '', '#dd4b39', '2018-02-05 00:00:00', '2018-02-05 00:00:00', 'true'),
+(26, 'PH/Cuti', '', '#dd4b39', '2018-02-07 00:00:00', '2018-02-07 00:00:00', 'true'),
+(27, 'PH/Cuti', '', '#dd4b39', '2018-02-21 00:00:00', '2018-02-21 00:00:00', 'true'),
+(28, 'Off', '', '#2f9550', '2018-01-29 00:00:00', '2018-01-29 00:00:00', 'true'),
+(29, 'Off', '', '#2f9550', '2018-01-30 00:00:00', '2018-01-30 00:00:00', 'true'),
+(30, 'Off', '', '#2f9550', '2018-01-31 00:00:00', '2018-01-31 00:00:00', 'true'),
+(31, 'PH/Cuti', '', '#dd4b39', '2018-02-04 00:00:00', '2018-02-04 00:00:00', 'true'),
+(32, 'Off', '', '#2f9550', '2018-02-06 00:00:00', '2018-02-06 00:00:00', 'true'),
+(33, 'PH/Cuti', '', '#dd4b39', '2018-02-12 00:00:00', '2018-02-12 00:00:00', 'true'),
+(34, 'PH/Cuti', '', '#dd4b39', '2018-03-02 00:00:00', '2018-03-02 00:00:00', 'true'),
+(35, 'PH/Cuti', '', '#dd4b39', '2018-02-09 00:00:00', '2018-02-09 00:00:00', 'true');
 
 -- --------------------------------------------------------
 
@@ -109,13 +127,11 @@ CREATE TABLE `tb_permintaan` (
 --
 
 INSERT INTO `tb_permintaan` (`id_permintaan`, `dari`, `departemen`, `catatan`, `status`, `tanggal`) VALUES
-('PK-00001', 'Iwan S', 'Marketing Commercial', 'Sambungkan Printer LAN ke PC', 'On Progress', '2018-02-14'),
-('PK-00002', 'Fajri', 'IT', 'Tolong pindahkan ruangan IT ke Basement segera', 'Finished', '0000-00-00'),
-('PK-00003', 'Arty', 'Marketing Commercial', 'Tolong pindahkan meja', 'Finished', '2018-02-14'),
-('PK-00004', 'Mardino Santosa', 'IT', 'pindah rumah', 'Finished', '2018-02-16'),
-('PK-00005', 'Dewi', 'Engineering', 'Komputer mati', 'On Progress', '2018-02-16'),
-('PK-00006', 'Mardino Santosa', 'IT', 'coba duylull', 'Waiting', '2018-02-17'),
-('PK-00007', 'Joni Iskandars', 'Marketing Commercial', 'tanggal 18 jonis', 'Finished', '2018-02-18');
+('PK-00001', 'Mardino Santosa', 'IT', 'Buat data baruu', 'Finished', '2018-02-20'),
+('PK-00002', 'Mardino Santosa', 'IT', 'erter werwerwe', 'Finished', '2018-02-20'),
+('PK-00003', 'Mardino Santosa', 'Food n Beverages', 'err', 'On Progress', '2018-02-20'),
+('PK-00005', 'Mardino Santosa', 'IT', 'rtyrty', 'Waiting', '2018-02-20'),
+('PK-00007', 'Mardino Santosa', 'Food n Beverages', 'ert', 'Finished', '2018-02-20');
 
 -- --------------------------------------------------------
 
@@ -136,10 +152,9 @@ CREATE TABLE `tb_solusi` (
 --
 
 INSERT INTO `tb_solusi` (`id_solusi`, `tanggal`, `solusi`, `status`, `oleh`) VALUES
-('PK-00002', '2018-02-17', 'selesai 100%', 'Finished', 'Mardino Santosa'),
-('PK-00003', '2018-02-15', 'sudah saya pindahkan bosss', 'Finished', 'Mardino Santosa'),
-('PK-00004', '2018-02-17', 'sudah diselesaikan, harusnya status 100%', 'Finished', 'Mardino Santosa'),
-('PK-00007', '2018-02-18', 'selesai', 'Finished', 'Mardino Santosa');
+('PK-00001', '2018-02-20', 'sudah selesai', 'Finished', 'Mardino Santosa'),
+('PK-00002', '2018-02-20', 'qweqwe', 'Finished', 'Mardino Santosa'),
+('PK-00007', '2018-02-20', 'selesaiii', 'Finished', 'Mardino Santosa');
 
 -- --------------------------------------------------------
 
@@ -161,7 +176,7 @@ CREATE TABLE `tb_user` (
 --
 
 INSERT INTO `tb_user` (`id_user`, `nama_user`, `pass_user`, `nama_lengkap`, `level`, `avatar`) VALUES
-(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'Mardino Santosa', '1', './avatar/user2-160x160.jpg'),
+(1, 'dino', 'a8f5f167f44f4964e6c998dee827110c', 'Mardino Santosass', '1', './avatar/avatar31.png'),
 (2, 'user', 'ee11cbb19052e40b07aac0ca060c23ee', 'user', '2', './avatar/avatar5.png');
 
 --
@@ -186,6 +201,12 @@ ALTER TABLE `tb_departemen`
 --
 ALTER TABLE `tb_identifikasi`
   ADD PRIMARY KEY (`id_identifikasi`);
+
+--
+-- Indexes for table `tb_jadwal`
+--
+ALTER TABLE `tb_jadwal`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `tb_permintaan`
@@ -213,7 +234,12 @@ ALTER TABLE `tb_user`
 -- AUTO_INCREMENT for table `tb_departemen`
 --
 ALTER TABLE `tb_departemen`
-  MODIFY `id_departemen` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id_departemen` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT for table `tb_jadwal`
+--
+ALTER TABLE `tb_jadwal`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 --
 -- AUTO_INCREMENT for table `tb_user`
 --
